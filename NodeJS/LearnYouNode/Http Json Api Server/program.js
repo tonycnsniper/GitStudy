ddd0b0c57407a051cfd6722238c97fb650f1243d
tonycnsniper <tony_sniper@163.com>
2014-08-07 10:08:33 +0800

    Tony - implement rout function for api parse

--- NodeJS/LearnYouNode/Http Json Api Server/program.js
+++ NodeJS/LearnYouNode/Http Json Api Server/program.js
@@ -1,1 +1,24 @@
 var http = require('http')
+var url = require('url')
+
+var server = http.createServer(function(request, response) {
+	var queryStr = url.parse(request.url, true)
+	
+	if(queryStr.pathname === '/api/parsetime') {
+		var date = new Date()
+		var obj = {
+			"hour" : date.getHours(),
+			"minute" : date.getMinutes(),
+			"second" : date.getSeconds()
+		}
+	}
+	else if(queryStr.pathname === '/api/unixtime') {
+		var date = new Date(UNIX_timestamp)
+		var obj = {
+			"unixtime" : date.toString()
+		}
+	}
+
+	response.writeHead(200, { 'Content-Type' : 'application/json' })
+	response.write(obj.toString())
+}).listen(process.argv[2])
