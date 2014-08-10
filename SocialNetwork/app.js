7783aa7a9c96c976619e7aa90baaebb4e8504eb2
tonycnsniper <tony_sniper@163.com>
2014-08-10 22:13:02 +0800

    Add Routing & Registration Part

--- SocialNetwork/app.js
+++ SocialNetwork/app.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var app = express();
+
 app.configure(function(){
-app.set('view engine', 'jade');
-app.use(express.static(__dirname + '/public'));
+	app.set('view engine', 'jade');
+	app.use(express.static(__dirname + '/public'));
 });
+
 app.get('/', function(req, res){
-res.render("index.jade", {layout:false});
+	res.render("index.jade", {layout:false});
 });
+
+app.get('/account/authenticated', function(req, res) {
+	if( req.session.loggedIn ) {
+		res.send(200);
+	} else {
+		res.send(401);
+	}
+})
+
 app.listen(8080);
--- SocialNetwork/public/js/SocialNet.js
+++ SocialNetwork/public/js/SocialNet.js
@@ -7,3 +7,34 @@ define(['views/index'], function(indexView) {
 		initialize : initialize
 	};
 })
+
+define(['router'], function(router) {
+	var initialize = function() {
+		checkLogin(runApplication);
+	};
+
+	var checkLogin = function(callback) {
+		$.ajax("/account/authenticated", {
+			method: "GET",
+			success: function() {
+				return callback(true);
+			},
+			error: function(data) {
+				return callback(false);
+			}
+		});
+	};
+
+	var runApplication = function(authenticated) {
+		if(!authenticated) {
+			window.location.hash = 'login';
+		} else {
+			window.location.hash = 'index';
+		}
+		Backbone.history.start();
+	};
+
+	return {
+		initialize: initialize
+	};
+});
