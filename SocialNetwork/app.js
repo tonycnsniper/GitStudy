285d96cb76b80f97feeb417841466ecf179ebc58
tonycnsniper <tony_sniper@163.com>
2014-08-11 20:57:24 +0800

    Tony - add register template

--- SocialNetwork/app.js
+++ SocialNetwork/app.js
@@ -16,6 +16,20 @@ app.get('/account/authenticated', function(req, res) {
 	} else {
 		res.send(401);
 	}
+});
+
+app.post('/register', function(req, res) {
+	var firstName = req.param('firstName', '');
+	var lastName = req.param('lastName', '');
+	var email = req.param('email', null);
+	var password = req.param('password', null);
+
+	if( null == email || null == password) {
+		res.send(400);
+		return;
+	}
+
+	Account.register(email, password, firstName, lastName);
 })
 
 app.listen(8080);
