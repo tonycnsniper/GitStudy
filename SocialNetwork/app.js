b578d5af31d4464c13ebb83c3f442d2bef04e45e
tonycnsniper <tony_sniper@163.com>
2014-08-12 22:26:22 +0800

    Add forgot password part

--- SocialNetwork/app.js
+++ SocialNetwork/app.js
@@ -32,4 +32,43 @@ app.post('/register', function(req, res) {
 	Account.register(email, password, firstName, lastName);
 })
 
+app.post('/login', function(req, res) {
+	console.log('login request');
+	var email = req.param('email', null);
+	var password = req.param('password', null);
+
+	if( null == email || email.length < 1 || null == password || password.length < 1) {
+		res.send(400);
+		return;
+	}
+
+	Account.login(email, password, function(success) {
+		if( !success ) {
+			res.send(401);
+			return
+		}
+		console.log('login was successful');
+		res.send(200);
+	});
+});
+
+app.post('/forgotpassword', function(req, res) {
+	var hostname = req.headers.host;
+	var resetPasswordUrl = 'http://' + hostname + '/resetPassword';
+	var email = req.param('email', null);
+
+	if( null == email || email.length < 1 ) {
+		res.send(400);
+		return;
+	}
+
+	Account.forgotPassword(email, resetPasswordUrl, function(success) {
+		if(success) {
+			res.send(200);
+		} else {
+			res.send(404);
+		}
+	});
+});
+
 app.listen(8080);
