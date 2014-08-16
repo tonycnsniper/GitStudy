fce9f2abd4d78b5485d2ada1c0e2d1293a9c707e
tonycnsniper <tony_sniper@163.com>
2014-08-16 08:47:56 +0800

    Account details page 79 above is over

---
+++ SocialNetwork/public/js/views/profile.js
@@ -0,0 +1,27 @@
+define(['SocialNetView', 'text!templates/profile.html', 'text!templates/Status.html', 'model/Status', 'views/Status'], 
+	function(SocialNetView, profileTemplate, statusTemplate, Status, StatusView) {
+		var profileView = SocialNetView.extend({
+			el: $('#content'),
+
+			initialize: function () {
+				this.model.bind('change', this.render, this);
+			},
+
+			render: function() {
+				this.$el.html(
+						_.template(profileTemplate, this.model.toJSON())
+					);
+
+				var statusCollection = this.model.get('status');
+				if( null != statusCollection ) {
+					_.each(statusCollection, function(statusJson) {
+						var statusModel = new Status(statusJson);
+						var statusHtml = (new StatusView({ model: statusModel })).render().el;
+						$(statusHtml).prependTo('.status_list').hide().fadeIn('show');
+					});
+				}
+			}
+		});
+
+		return profileView;
+	});
