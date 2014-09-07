457abecdeb770d841fc4aa782cfc8c0eb3808106
tonycnsniper <tony_sniper@163.com>
2014-09-07 23:37:28 +0800

    add photo uploader code

--- Node JS In Action/photo/app.js
+++ Node JS In Action/photo/app.js
@@ -11,8 +11,12 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.configure(function() {
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'ejs');
+    app.set('photos', __dirname + '/public/photos');  
+})
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -24,6 +28,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+app.use('/upload', photos.form);
+app.use('/upload', photos.submit(app.get('photos')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ Node JS In Action/photo/routes/photos.js
@@ -0,0 +1,30 @@
+var path = require('path')
+var fs = require('fs')
+var join = path.join;
+var Photo = require('../models/Photo');
+
+exports.submit = function(dir) {
+	return function(request, response, next) {
+		var image = reqest.files.photo.image;
+		var name = reqest.body.photo.name || image.name;
+		var path = join(dir, image.name);
+
+		fs.rename(image.path, path, function(err) {
+			if(err) return next(err);
+
+			Photo.create({
+				name : name,
+				path : img.name
+			}, function(err) {
+				if(err) return next(err);
+				response.redirect('/');
+			})
+		})
+	}
+}
+
+exports.form = function(request, response) {
+	response.render('photos/upload', {
+		title: 'Photo upload'
+	});
+}
