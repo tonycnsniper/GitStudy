b61099a6cecb8c2eeb64c3311c8c2299023c6114
tonycnsniper <tony_sniper@163.com>
2014-09-25 09:05:45 +0800

    Body parser component of connect middleware

--- Node JS In Action/connect built-in middleware/app.js
+++ Node JS In Action/connect built-in middleware/app.js
@@ -1,13 +1,15 @@
 var connect = require('connect');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
 var app = connect()
 	.use(cookieParser('Just a cookie test!!!'))
 	.use(function(request, response) {
 		response.setHeader('Set-Cookie','foo=bar');
 		response.setHeader('Set-Cookie','tobi=ferret; Expires=True, 25 Sep 2014 12:30:13 GMT');
+		//console.log(request.cookie);
 		response.end('Hello world\n');
    })
 	.use(function(request, response){
-		console.log(request.cookie);
+		//console.log(request.cookie);
 	}).listen(3000)
