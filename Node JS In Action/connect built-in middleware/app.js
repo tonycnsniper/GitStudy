eeee04a5bf04016ef4fc8bda9cbe8410fe5fabf6
tonycnsniper <tony_sniper@163.com>
2014-09-29 09:15:07 +0800

    Update node js runtime version to 3.x

--- Node JS In Action/connect built-in middleware/app.js
+++ Node JS In Action/connect built-in middleware/app.js
@@ -1,15 +1,31 @@
 var connect = require('connect');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var http = require('http');
+// var cookieParser = require('cookie-parser');
+// var bodyParser = require('body-parser');
+
+// var app = connect()
+// 	//.use(cookieParser('Just a cookie test!!!'))
+// 	//.use(function(request, response) {
+// 	//	response.setHeader('Set-Cookie','foo=bar');
+// 		response.setHeader('Set-Cookie','tobi=ferret; Expires=True, 25 Sep 2014 12:30:13 GMT');
+// 		//console.log(request.cookie);
+// 		response.end('Hello world\n');
+//    })
+// 	.use(function(request, response){
+// 		//console.log(request.cookie);
+// 	})
+// 	.use(bodyParser('Just a body parser here!'))
+// 	.use(function(request, response){
+		
+// 	}).listen(3000)
 
 var app = connect()
-	.use(cookieParser('Just a cookie test!!!'))
-	.use(function(request, response) {
-		response.setHeader('Set-Cookie','foo=bar');
-		response.setHeader('Set-Cookie','tobi=ferret; Expires=True, 25 Sep 2014 12:30:13 GMT');
-		//console.log(request.cookie);
-		response.end('Hello world\n');
-   })
-	.use(function(request, response){
-		//console.log(request.cookie);
-	}).listen(3000)
+	.use(connect.logger())
+	.use(hello);
+
+
+function hello(response, response, next) {
+	response.end('hello\n');
+}
+
+http.createServer(app).listen(3000);
